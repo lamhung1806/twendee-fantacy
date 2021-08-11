47dfe1da4743a2c619160bf67a19886b367356e8
HocVuVan <van.hocvu@mission.plus>
2021-08-12 00:57:04 +0700

    create base project

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+import bgHeader from "../../assets/images/bg-header.svg";
+function App() {
+  return (
+    <div className="App">
+      <img src={bgHeader} alt="" />
+    </div>
+  );
+}
+
+export default App;
