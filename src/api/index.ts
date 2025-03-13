3d83b78a6d8288927b7f2f7f09c0529bf6259e97
Hunglv <59758511+lamhung1806@users.noreply.github.com>
2025-03-13 15:30:15 +0700

    change url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: `dev-server.twendeesoft.com/fantacy/be/api/`,
+  baseURL: `https://dev-server.twendeesoft.com/fantacy/be/api/`,
 });
 
 export default instance;
